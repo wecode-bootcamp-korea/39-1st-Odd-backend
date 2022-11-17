82c164da358283c0591382f57839a1dff6ebd858
hosose <hosose22@gmail.com>
2022-11-17 11:24:29 +0900

    초기환경 세팅 (리뷰 반영)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
 // const userRouter = require("./userRouter");
-const lectureRouter = require("./lectureRouter");
+
 const router = express.Router();
 
 // router.use("/users", userRouter);
-router.use("/lectures", lectureRouter);
 
 module.exports = router;
