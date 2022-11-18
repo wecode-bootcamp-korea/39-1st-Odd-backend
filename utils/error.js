632bfb852b05dc3f720b416b3819e6a2b33b6d0e
hosose <hosose22@gmail.com>
2022-11-18 18:47:33 +0900

    멘토님 리뷰 반영 및 error 핸들링 미들웨어 추가

--- utils/error.js
+++ utils/error.js
@@ -1,9 +1,15 @@
-const raiseCustomError = (message, statusCode) => {
-  const err = new Error(message);
-  err.statusCode = statusCode;
-  throw err;
+const catchAsync = (func) => {
+  return (req, res, next) => {
+    func(req, res, next).catch((error) => next(error));
+  };
 };
 
-module.exports = {
-  raiseCustomError,
+const globalErrorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({ message: err.message });
 };
+
+module.exports = { catchAsync, globalErrorHandler };
