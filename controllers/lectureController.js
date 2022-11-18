2a47bfa7af9c822a532c97f9788bc751060617e0
hosose <hosose22@gmail.com>
2022-11-18 21:45:04 +0900

    멘토님 리뷰 반영 및 error 핸들링 미들웨어 추가

--- controllers/lectureController.js
+++ controllers/lectureController.js
@@ -1,17 +1,13 @@
 const lectureService = require("../services/lectureService");
+const { catchAsync } = require("../utils/error");
 
-const getLectureByLectureId = async (req, res) => {
-  try {
-    const lectureId = +req.params.lectureId;
+const getLectureByLectureId = catchAsync(async (req, res) => {
+  const lectureId = +req.params.lectureId;
 
-    const lectures = await lectureService.getLectureByLectureId(lectureId);
+  const lectures = await lectureService.getLectureByLectureId(lectureId);
 
-    return res.status(200).json(lectures);
-  } catch (err) {
-    console.log(err);
-    return res.status(err.statusCode || 500).json({ message: err.message });
-  }
-};
+  return res.status(200).json(lectures);
+});
 
 module.exports = {
   getLectureByLectureId,
