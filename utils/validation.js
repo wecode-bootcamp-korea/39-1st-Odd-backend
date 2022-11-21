3a0e36b3f4f5c08e2950147c58c95149b6cbb040
KHS <dev0226@gmail.com>
2022-11-21 17:17:53 +0900

    fixed validation.js

--- utils/validation.js
+++ utils/validation.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const validatePw = (password) => {
-    const pwValidation = new RegExp(
-      "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
-    );
-    if (!pwValidation.test(password)) {
-      const err = new Error("PASSWORD_IS_NOT_VALID");
-      err.statusCode = 409;
-      throw err;
-    }
-  };
-
-const validateEmail = (email) => {
-    const re = new RegExp(
-      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
-    );
-    if (!re.test(email)) {
-      const err = new Error("invalid email");
-      err.statusCode = 400;
-      throw err;
-    }
-  };
-
-  module.exports = {
-    validatePw, validateEmail
-  }
-=======
 const validateEmail = (email) => {
   const emailValidation = new RegExp(
     /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
@@ -50,4 +23,3 @@ const validatePw = (password) => {
 };
 
 module.exports = { validateEmail, validatePw };
->>>>>>> main
