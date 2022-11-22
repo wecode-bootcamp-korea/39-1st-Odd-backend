098648ee42a41538bd6431b23976c712d0a3a86f
hosose <hosose22@gmail.com>
2022-11-22 16:37:06 +0900

    중복된 product 추가 불가 기능

--- models/cartDao.js
+++ models/cartDao.js
@@ -19,4 +19,29 @@ const addProduct = async (userId, productId, quantity) => {
   }
 };
 
-module.exports = { addProduct };
+const getCartByUserId = async (userId) => {
+  try {
+    return await database.query(
+      `SELECT
+        C.id,
+        C.product_id as productId,
+        C.quantity,
+        P.title,
+        P.thumbnail_image_url
+      FROM
+        carts C
+      LEFT JOIN
+        products P
+      ON
+        P.id = C.product_id
+      WHERE
+        C.user_id = ?
+      `,
+      [userId]
+    );
+  } catch (err) {
+    raiseCustomError("INVALID_DATA_INPUT", 500);
+  }
+};
+
+module.exports = { addProduct, getCartByUserId };
--- services/cartService.js
+++ services/cartService.js
@@ -4,6 +4,14 @@ const addProduct = async (userId, productId, quantity) => {
   if (!quantity) {
     quantity = 1;
   }
+
+  const cartProduct = await cartDao.getCartByUserId(userId);
+  for (let i = 0; i < cartProduct.length; i++) {
+    if (cartProduct[i].productId == productId) {
+      raiseCustomError("Already added", 409);
+    }
+  }
+
   return await cartDao.addProduct(userId, productId, quantity);
 };
 
