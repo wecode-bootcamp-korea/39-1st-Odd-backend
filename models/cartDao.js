8ee882028da817a6136c26850e6dbb730743f71c
hosose <hosose22@gmail.com>
2022-11-23 14:25:58 +0900

    client와 연결 성공

--- models/cartDao.js
+++ models/cartDao.js
@@ -6,16 +6,26 @@ const getCartByUserId = async (userId) => {
     return await database.query(
       `SELECT
           C.id,
-          C.product_id,
+          C.product_id AS productId,
           C.quantity,
           P.title,
-          P.thumbnail_image_url
+          P.thumbnail_image_url AS image,
+          PT.name AS type,
+          P.price
         FROM
           carts C
         LEFT JOIN
           products P
         ON
           P.id = C.product_id
+        LEFT JOIN
+          categories AS CA
+        ON 
+          CA.id = P.category_id
+        LEFT JOIN
+          product_types AS PT
+        ON
+          PT.id = CA.product_type_id
         WHERE
           C.user_id = ?
         `,
