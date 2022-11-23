e91f11347a5c562bbdfb11ab010bd78675b30afb
hosose <hosose22@gmail.com>
2022-11-23 17:48:35 +0900

    오타 수정

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const userRouter = require("./userRouter");
-const cartRouter = require("./cartRouter");
 
 const router = express.Router();
 
+const userRouter = require("./userRouter");
+const lectureRouter = require("./lectureRouter");
+const cartRouter = require("./cartRouter");
+
 router.use("/users", userRouter);
+router.use("/lecture", lectureRouter);
 router.use("/cart", cartRouter);
 
 module.exports = router;
