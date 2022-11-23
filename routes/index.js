9a4264340c994818cfd9998a70abb78231166647
hosose <hosose22@gmail.com>
2022-11-23 17:05:03 +0900

    오타 발견

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./userRouter");
+const lectureRouter = require("./lectureRouter");
 
 router.use("/users", userRouter);
+router.use("/lectures", lectureRouter);
 
 module.exports = router;
