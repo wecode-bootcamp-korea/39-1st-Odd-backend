f16d4b5bdad70b140ddeef46ef45bd87c86448a9
hosose <hosose22@gmail.com>
2022-11-23 18:08:36 +0900

    오타 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,7 @@ const signIn = catchAsync(async (req, res) => {
   const { email, password } = req.body;
 
   const accessToken = await userService.signIn(email, password);
-  res.status(200).json({ accsessToken: accessToken });
+  res.status(200).json({ accessToken: accessToken });
 });
 
 module.exports = {
