c8d1ad9464eb5a8167f05dd966d23071b1c25db6
hosose <hosose22@gmail.com>
2022-11-24 14:03:20 +0900

    멘토님 리뷰 반영

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,13 +3,10 @@ const { catchAsync, raiseCustomError } = require("../utils/error");
 
 const getCartByUserId = catchAsync(async (req, res) => {
   const userId = req.user.id;
-  if (!userId) {
-    raiseCustomError("KEY_ERROR", 400);
-  }
 
-  const cart = await cartService.getCartByUserId(userId);
+  const carts = await cartService.getCartByUserId(userId);
 
-  return res.status(200).json(cart);
+  return res.status(200).json(carts);
 });
 
 module.exports = { getCartByUserId };
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -5,6 +5,6 @@ const { loginRequired } = require("../utils/checkUser");
 
 const router = express.Router();
 
-router.get("/", loginRequired, cartController.getCartByUserId);
+router.get("", loginRequired, cartController.getCartByUserId);
 
 module.exports = router;
