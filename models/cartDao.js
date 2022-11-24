4c805b1d66ccac02ad9bbe8eb04610830c645cef
hosose <hosose22@gmail.com>
2022-11-24 11:37:58 +0900

    장바구니 data 불러오기 수정

--- models/cartDao.js
+++ models/cartDao.js
@@ -5,29 +5,36 @@ const getCartByUserId = async (userId) => {
   try {
     return await database.query(
       `SELECT
-          C.id,
-          C.product_id AS productId,
-          C.quantity,
-          P.title,
-          P.thumbnail_image_url AS image,
-          PT.name AS type,
-          P.price
-        FROM
-          carts C
-        LEFT JOIN
-          products P
-        ON
-          P.id = C.product_id
-        LEFT JOIN
-          categories AS CA
-        ON 
-          CA.id = P.category_id
-        LEFT JOIN
-          product_types AS PT
-        ON
-          PT.id = CA.product_type_id
-        WHERE
-          C.user_id = ?
+        U.name,
+        U.email,
+        U.phone_number AS phoneNumber,
+        C.id,
+        C.product_id as productId,
+        C.quantity,
+        P.title,
+        P.thumbnail_image_url AS image,
+        PT.name AS type,
+        P.price
+      FROM
+        users U
+      LEFT JOIN
+        carts C
+      ON
+        U.id = C.user_id
+      LEFT JOIN
+        products P
+      ON
+        P.id = C.product_id
+      LEFT JOIN
+        categories AS CA
+      ON 
+        CA.id = P.category_id
+      LEFT JOIN
+        product_types AS PT
+      ON
+        PT.id = CA.product_type_id
+      WHERE
+        U.id = ?
         `,
       [userId]
     );
