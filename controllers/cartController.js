244bfcb348043dbc0c24ca5f061b27072f98147f
hosose <hosose22@gmail.com>
2022-11-24 13:57:31 +0900

    멘토님 리뷰 반영

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,7 +4,7 @@ const { catchAsync, raiseCustomError } = require("../utils/error");
 const addCart = catchAsync(async (req, res) => {
   const userId = req.user.id;
   const productId = req.params.productId;
-  const { quantity } = req.body;
+  let { quantity } = req.body;
 
   if (!userId || !productId) {
     raiseCustomError(KEY_ERROR, 400);
